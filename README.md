dc0d000f5da1512d961db80b3bbaeebcd7b5ad54
Jordan Seiler <jordan.seiler@ozmoapp.com>
2024-09-05 09:58:37 -0400

    Add lualine and whichkey

---
+++ README.md
@@ -0,0 +1,18 @@
+# Usage Requirements:
+1. Neovim 0.9 or greater
+2. A version of Neovim with LuaJIT enabled
+3. `ripgrep` :: telescope
+4. `fd` :: telescope
+5. A patched NerdFont :: lualine
+6. nvim-web-devicons :: Dependency of lualine but also used by
+   which-key.
+
+To set up this config on a new machine:
+1. `mkdir ~/.config/nvim`
+2. `cd ~/.config/nvim`
+3. Clone this repository
+4. Launch Neovim
+
+If the Neovim config directory already exists. Save a copy of it, delete the
+contents of the directory, and follow the steps above from step 2.
+
--- init.lua
+++ init.lua
@@ -1,12 +1,32 @@
-require("config.lazy")
+require('config.lazy')
+require('mini.icons').setup()
 
-vim.o.background = 'dark' -- or "light" for light mode
+-- Leader and LocalLeader are SPACE and \\ respectively
+-- Both are configured in lazy.lua
+
+-- BASIC CONFIGURATION
+vim.opt.background = 'dark' -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'number'
 
+-- Tab = 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+
+-- ## lualine
+require('lualine').setup {
+    sections = {
+        lualine_y = {'searchcount'},
+    } 
+}
+
+-- TELESCOPE BINDINGS
 local ts_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', ts_builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', ts_builtin.buffers, {})
@@ -18,4 +38,5 @@ vim.keymap.set('n', '<leader>fj', ts_builtin.jumplist, {})
 vim.keymap.set('n', '<leader>fz', ts_builtin.current_buffer_fuzzy_find, {})
 -- 's' for 'symbols'
 vim.keymap.set('n', '<leader>fs', ts_builtin.treesitter, {})
+
 

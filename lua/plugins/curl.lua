996571263f50478e05ecdf3043989157bcac9943
Jordan Seiler <jseiler@twilio.com>
2025-11-26 14:20:38 -0500

    Add curl.nvim

---
+++ lua/plugins/curl.lua
@@ -0,0 +1,9 @@
+return {
+  "oysandvik94/curl.nvim",
+  commit="3ee14fbafc8169fc803e80562ce7ac5b4474bdff",
+  cmd = { "CurlOpen" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = true
+}

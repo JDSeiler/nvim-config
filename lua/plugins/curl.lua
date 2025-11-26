70cad9241e11fd89617ac22d306d15c326349cdc
Jordan Seiler <jseiler@twilio.com>
2025-11-26 15:25:48 -0500

    misc curl config changes

--- lua/plugins/curl.lua
+++ lua/plugins/curl.lua
@@ -1,9 +1,22 @@
+-- See: https://github.com/oysandvik94/curl.nvim/issues/67#issuecomment-3344107357
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.curl",
+  callback = function()
+    vim.opt.filetype = "curl"
+  end,
+})
+
 return {
   "oysandvik94/curl.nvim",
   commit="3ee14fbafc8169fc803e80562ce7ac5b4474bdff",
   cmd = { "CurlOpen" },
+  ft = { "curl" },
   dependencies = {
     "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    open_with = "buffer"
   },
   config = true
 }
